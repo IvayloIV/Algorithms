dd49b3b13420373737cb4cd7cdd6ed92bc6a2eea
IvayloIV <ivvo98@abv.bg>
2018-12-14 23:11:43 +0200

    Add Lab - Graphs and Graph Algorithms

---
+++ Algorithms-July-2017/Lab-Graphs_and_Graph_Algorithms/01. Connected-Components/GraphConnectedComponents.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class GraphConnectedComponents
+{
+    static List<int>[] graph;
+    static bool[] visited;
+
+    public static void Main()
+    {
+        graph = ReadGraph();
+        FindGraphConnectedComponents();
+    }
+
+    private static List<int>[] ReadGraph()
+    {
+        int n = int.Parse(Console.ReadLine());
+        visited = new bool[n];
+        var graph = new List<int>[n];
+        for (int i = 0; i < n; i++)
+        {
+            graph[i] = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToList();
+        }
+        return graph;
+    }
+
+    private static void FindGraphConnectedComponents()
+    {
+        for (int i = 0; i < graph.Length; i++)
+        {
+            if (!visited[i])
+            {
+                Console.Write("Connected component:");
+                DFS(i);
+                Console.WriteLine();
+            }
+        }
+    }
+
+    private static void DFS(int n)
+    {
+        visited[n] = true;
+
+        foreach (var node in graph[n])
+        {
+            if (!visited[node])
+            {
+                DFS(node);
+            }
+        }
+
+        Console.Write($" {n}");
+    }
+}
---
+++ Algorithms-July-2017/Lab-Graphs_and_Graph_Algorithms/02. Topological-Sorting/TopologicalSorter.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class TopologicalSorter
+{
+    private Dictionary<string, List<string>> graph;
+    private Dictionary<string, int> elements;
+    private HashSet<string> visited;
+    private HashSet<string> circles;
+
+    public TopologicalSorter(Dictionary<string, List<string>> graph)
+    {
+        this.graph = graph;
+        this.visited = new HashSet<string>();
+        this.circles = new HashSet<string>();
+    }
+
+    public ICollection<string> TopSortDFS()
+    {
+        var result = new LinkedList<string>();
+        DFS(result, graph.First().Key);
+        return result;
+    }
+
+    private void DFS(LinkedList<string> result, string key)
+    {
+        this.circles.Add(key);
+        visited.Add(key);
+
+        foreach (var child in graph[key])
+        {
+            if (this.circles.Contains(child))
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (!visited.Contains(child))
+            {
+                DFS(result, child);
+            }
+        }
+
+        this.circles.Remove(key);
+        result.AddFirst(key);
+    }
+
+    public ICollection<string> TopSort()
+    {
+        var result = new List<string>();
+        GetElements();
+
+        while (true)
+        {
+            var withoutEdges = this.elements.Where(a => a.Value == 0);
+
+            if (withoutEdges.Count() == 0)
+            {
+                break;
+            }
+
+            var firstNode = withoutEdges.First();
+
+            this.elements[firstNode.Key]--;
+            foreach (var child in graph[firstNode.Key])
+            {
+                elements[child]--;
+            }
+
+            this.graph.Remove(firstNode.Key);
+            result.Add(firstNode.Key);
+        }
+
+        if (this.graph.Count != 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        return result;
+    }
+
+    private void GetElements()
+    {
+        this.elements = new Dictionary<string, int>();
+
+        foreach (var kvp in graph)
+        {
+            var node = kvp.Key;
+            var children = kvp.Value;
+
+            if (!elements.ContainsKey(node))
+            {
+                elements[node] = 0;
+            }
+
+            foreach (var child in children)
+            {
+                if (!elements.ContainsKey(child))
+                {
+                    elements[child] = 0;
+                }
+
+                elements[child]++;
+            }
+        }
+    }
+}
