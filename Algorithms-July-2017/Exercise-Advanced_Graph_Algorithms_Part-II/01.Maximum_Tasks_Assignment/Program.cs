5fc2dc4165a91a77bc5aa135cca9317edde989af
IvayloIV <ivvo98@abv.bg>
2018-12-24 00:39:47 +0200

    Add Exercise - Advanced Graph Algorithms Part-II

---
+++ Algorithms-July-2017/Exercise-Advanced_Graph_Algorithms_Part-II/01.Maximum_Tasks_Assignment/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.Maximum_Tasks_Assignment
+{
+    class Program
+    {
+        static int[] parents;
+
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine().Split()[1]);
+            int tasks = int.Parse(Console.ReadLine().Split()[1]);
+            int[,] grahp = new int[people + tasks + 2, people + tasks + 2];
+
+            for (int i = 1; i <= people; i++)
+            {
+                grahp[0, i] = 1;
+            }
+
+            for (int i = 1; i <= tasks; i++)
+            {
+                grahp[grahp.GetLength(1) - 1 - i, grahp.GetLength(0) - 1] = 1;
+            }
+
+            for (int row = 1; row <= tasks; row++)
+            {
+                char[] line = Console.ReadLine().ToCharArray();
+                for (int col = 1; col <= people; col++)
+                {
+                    if (line[col - 1] == 'Y')
+                    {
+                        grahp[row, people + col] = 1;
+                    }
+                }
+            }
+
+            int start = 0;
+            int end = grahp.GetLength(0) - 1;
+
+            while (BFS(start, end, grahp))
+            {
+                int current = grahp.GetLength(0) - 1;
+
+                while (current != start)
+                {
+                    grahp[parents[current], current] = 0;
+                    grahp[current, parents[current]] = 1;
+                    current = parents[current];
+                }
+            }
+
+            bool[] visited = new bool[grahp.GetLength(0)];
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(end);
+            visited[end] = true;
+
+            SortedSet<string> result = new SortedSet<string>();
+            while (queue.Count > 0)
+            {
+                int current = queue.Dequeue();
+
+                for (int i = 0; i < grahp.GetLength(1); i++)
+                {
+                    if (!visited[i] && grahp[current, i] != 0)
+                    {
+                        visited[i] = true;
+                        queue.Enqueue(i);
+                        grahp[current, i] = 0;
+
+                        if (current != start && current != end && i != start && i != end)
+                        {
+                            result.Add($"{(char)(i + 'A' - 1)}-{current - people}");
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, result));
+        }
+
+        private static bool BFS(int start, int end, int[,] grahp)
+        {
+            bool[] visited = new bool[grahp.GetLength(0)];
+            parents = new int[grahp.GetLength(0)];
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(start);
+            visited[start] = true;
+
+            while (queue.Count > 0)
+            {
+                int current = queue.Dequeue();
+
+                for (int i = grahp.GetLength(1) - 1; i >= 0; i--)
+                {
+                    if (!visited[i] && grahp[current, i] > 0)
+                    {
+                        visited[i] = true;
+                        queue.Enqueue(i);
+                        parents[i] = current;
+                    }
+                }
+            }
+
+            return visited[end];
+        }
+    }
+}
---
+++ Algorithms-July-2017/Exercise-Advanced_Graph_Algorithms_Part-II/02.Find_Bi-Connected_Components/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.Find_Bi_Connected_Components
+{
+    class Program
+    {
+        static List<int>[] graph;
+        static int[] parents;
+        static int[] depth;
+        static int[] lowpoint;
+        static bool[] visited;
+        static Stack<KeyValuePair<int, int>> stack;
+        static int counter;
+
+        static void Main(string[] args)
+        {
+            int nodes = int.Parse(Console.ReadLine().Split()[1]);
+            int edges = int.Parse(Console.ReadLine().Split()[1]);
+
+            graph = new List<int>[nodes];
+
+            for (int i = 0; i < edges; i++)
+            {
+                int[] tokens = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                AddNewPoint(tokens[0], tokens[1]);
+                AddNewPoint(tokens[1], tokens[0]);
+            }
+
+            parents = new int[nodes];
+            depth = new int[nodes];
+            lowpoint = new int[nodes];
+            visited = new bool[nodes];
+            stack = new Stack<KeyValuePair<int, int>>();
+            FindBiConnectedComponents(0, 0);
+
+            Console.WriteLine($"Number of bi-connected components: {counter}");
+        }
+
+        private static void FindBiConnectedComponents(int node, int currentDepth)
+        {
+            visited[node] = true;
+            depth[node] = currentDepth;
+            lowpoint[node] = currentDepth;
+
+            foreach (var child in graph[node])
+            {
+                if (!visited[child])
+                {
+                    parents[child] = node;
+                    FindBiConnectedComponents(child, currentDepth + 1);
+
+                    stack.Push(new KeyValuePair<int, int>(node, child));
+                    if (depth[node] <= lowpoint[child])
+                    {
+                        if (stack.Count > 0)
+                        {
+                            var edge = stack.Peek();
+                            do
+                            {
+                                edge = stack.Pop();
+                            } while (stack.Count > 0 && (edge.Key != node || stack.Peek().Key == edge.Value));
+                            counter++;
+                        }
+                    }
+
+                    lowpoint[node] = Math.Min(lowpoint[node], lowpoint[child]);
+                }
+                else if (parents[node] != child)
+                {
+                    lowpoint[node] = Math.Min(lowpoint[node], depth[child]);
+                }
+            }
+        }
+
+        private static void AddNewPoint(int start, int end)
+        {
+            if (graph[start] == null)
+            {
+                graph[start] = new List<int>();
+            }
+
+            graph[start].Add(end);
+        }
+    }
+}
