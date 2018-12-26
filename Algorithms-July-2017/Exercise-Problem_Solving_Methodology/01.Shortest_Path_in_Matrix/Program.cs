ad5bef1edbdedbd01a093a9fccde0f0c216edd0d
IvayloIV <ivvo98@abv.bg>
2018-12-26 14:11:10 +0200

    Add Exercise - Problem Solving Methodology

---
+++ Algorithms-July-2017/Exercise-Problem_Solving_Methodology/01.Shortest_Path_in_Matrix/Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Wintellect.PowerCollections;
+
+namespace _01.Shortest_Path_in_Matrix
+{
+    class Program
+    {
+        static int[,] matrix;
+        static Dictionary<int, Node> graph;
+        static Dictionary<int, Node> prev;
+
+        static void Main(string[] args)
+        {
+            int rowsCount = int.Parse(Console.ReadLine());
+            int colsCount = int.Parse(Console.ReadLine());
+
+            int nodesCount = rowsCount * colsCount;
+            matrix = new int[rowsCount, colsCount];
+            int[,] countMatrix = new int[rowsCount, colsCount];
+            graph = new Dictionary<int, Node>();
+
+            FillMatrix(rowsCount, colsCount);
+            FillGraph(rowsCount, colsCount);
+
+            int start = 0;
+            int end = graph.Count - 1;
+            FindShortestPath(start, end);
+            PrintPath(start, end);
+        }
+
+        private static void FillGraph(int rowsCount, int colsCount)
+        {
+            for (int row = 0; row < rowsCount; row++)
+            {
+                for (int col = 0; col < colsCount; col++)
+                {
+                    int currentCell = row * colsCount + col;
+                    AddToGraph(currentCell, colsCount, row, col);
+                }
+            }
+        }
+
+        private static void PrintPath(int start, int end)
+        {
+            Console.WriteLine($"Length: " + graph[graph.Count - 1].Weight);
+
+            Stack<int> path = new Stack<int>();
+            int current = end;
+            while (current != start)
+            {
+                Node node = graph[current];
+                path.Push(matrix[node.Row, node.Col]);
+                current = prev[current].Value;
+            }
+            Node startNode = graph[start];
+            path.Push(matrix[startNode.Row, startNode.Col]);
+
+            Console.WriteLine($"Path: {string.Join(" ", path)}");
+        }
+
+        private static void FillMatrix(int rowsCount, int colsCount)
+        {
+            for (int row = 0; row < rowsCount; row++)
+            {
+                int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < colsCount; col++)
+                {
+                    matrix[row, col] = nums[col];
+                }
+            }
+        }
+
+        private static void FindShortestPath(int start, int end)
+        {
+            OrderedBag<Node> priorityQueue = new OrderedBag<Node>();
+            priorityQueue.Add(graph[start]);
+            bool[] visited = new bool[graph.Count];
+            visited[start] = true;
+            Dictionary<int, int> oldWeights = new Dictionary<int, int>();
+            oldWeights[start] = graph[start].Weight;
+            prev = new Dictionary<int, Node>();
+
+            while (priorityQueue.Count > 0)
+            {
+                Node current = priorityQueue.RemoveFirst();
+
+                foreach (var child in current.Children)
+                {
+                    if (!visited[child.Value])
+                    {
+                        visited[child.Value] = true;
+                        oldWeights[child.Value] = child.Weight;
+                        child.Weight = child.Weight + current.Weight;
+                        prev[child.Value] = current;
+                        priorityQueue.Add(child);
+                    }
+                    else if (oldWeights[child.Value] + current.Weight <= child.Weight)
+                    {
+                        child.Weight = oldWeights[child.Value] + current.Weight;
+                        prev[child.Value] = current;
+                    }
+                }
+            }
+        }
+
+        private static void AddToGraph(int currentCell, int colsCount, int row, int col)
+        {
+            if (!graph.ContainsKey(currentCell))
+            {
+                Node newNode = new Node(currentCell, matrix[row, col], row, col);
+                graph[currentCell] = newNode;
+            }
+
+            AddChild(currentCell, currentCell + 1, row, col + 1);
+            AddChild(currentCell, currentCell - 1, row, col - 1);
+            AddChild(currentCell, currentCell + colsCount, row + 1, col);
+            AddChild(currentCell, currentCell - colsCount, row - 1, col);
+        }
+
+        private static void AddChild(int currentCell, int node, int row, int col)
+        {
+            if (!IsInside(row, col))
+            {
+                return;
+            }
+
+            Node child;
+            if (graph.ContainsKey(node))
+            {
+                child = graph[node];
+            }
+            else
+            {
+                child = new Node(node, matrix[row, col], row, col);
+                graph[node] = child;
+            }
+
+            graph[currentCell].Children.Add(child);
+        }
+
+        private static bool IsInside(int row, int col)
+        {
+            return row >= 0 && col >= 0 && row < matrix.GetLength(0) && col < matrix.GetLength(1);
+        }
+    }
+
+    class Node : IComparable<Node>
+    {
+        public int Value { get; set; }
+
+        public int Weight { get; set; }
+
+        public List<Node> Children { get; set; }
+
+        public int Row { get; set; }
+
+        public int Col { get; set; }
+
+        public Node(int value, int weight, int row, int col)
+        {
+            this.Value = value;
+            this.Weight = weight;
+            this.Children = new List<Node>();
+            this.Row = row;
+            this.Col = col;
+        }
+
+        public int CompareTo(Node other)
+        {
+            return this.Weight.CompareTo(other.Weight);
+        }
+    }
+}
---
+++ Algorithms-July-2017/Exercise-Problem_Solving_Methodology/02.Rectangle_Intersection/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.Rectangle_Intersection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            long totalArea = 0;
+
+            List<Rectangle> rectangles = new List<Rectangle>();
+            List<int> x = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                int[] line = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                x.Add(line[0]);
+                x.Add(line[1]);
+                rectangles.Add(new Rectangle(line[0], line[1], line[2], line[3]));
+            }
+            x.Sort();
+            List<List<Rectangle>> rectanglesX = GetXRectangles(rectangles, x);
+
+            for (int i = 0; i < rectanglesX.Count - 1; i++)
+            {
+                var current = rectanglesX[i];
+                if (current.Count < 2)
+                {
+                    continue;
+                }
+
+                List<int> y = new List<int>();
+
+                foreach (var rectangle in current)
+                {
+                    y.Add(rectangle.MinY);
+                    y.Add(rectangle.MaxY);
+                }
+
+                y.Sort();
+                int[] overlappings = GetOverlappings(current, y);
+                totalArea = CalculateArea(totalArea, x, i, y, overlappings);
+            }
+
+            Console.WriteLine(totalArea);
+        }
+
+        private static long CalculateArea(long totalArea, List<int> x, int i, List<int> y, int[] overlappings)
+        {
+            for (int j = 0; j < overlappings.Length; j++)
+            {
+                if (overlappings[j] >= 2)
+                {
+                    int diffX = x[i + 1] - x[i];
+                    int diffY = y[j + 1] - y[j];
+                    totalArea += diffX * diffY;
+                }
+            }
+
+            return totalArea;
+        }
+
+        private static int[] GetOverlappings(List<Rectangle> current, List<int> y)
+        {
+            int[] overlappings = new int[y.Count - 1];
+            foreach (var rectangle in current)
+            {
+                for (int j = 0; j < y.Count - 1; j++)
+                {
+                    if (rectangle.MinY < y[j + 1] && rectangle.MaxY > y[j])
+                    {
+                        overlappings[j]++;
+                    }
+                }
+            }
+
+            return overlappings;
+        }
+
+        private static List<List<Rectangle>> GetXRectangles(List<Rectangle> rectangles, List<int> x)
+        {
+            List<List<Rectangle>> rectanglesX = new List<List<Rectangle>>();
+            for (int i = 0; i < x.Count - 1; i++)
+            {
+                rectanglesX.Add(new List<Rectangle>());
+            }
+
+            foreach (var rectangle in rectangles)
+            {
+                for (int i = 0; i < x.Count - 1; i++)
+                {
+                    if (rectangle.MinX < x[i + 1] && rectangle.MaxX > x[i])
+                    {
+                        rectanglesX[i].Add(rectangle);
+                    }
+                }
+            }
+
+            return rectanglesX;
+        }
+    }
+
+    class Rectangle
+    {
+        public int MinX { get; set; }
+
+        public int MaxX { get; set; }
+
+        public int MinY { get; set; }
+
+        public int MaxY { get; set; }
+
+        public Rectangle(int minX, int maxX, int minY, int maxY)
+        {
+            this.MinX = minX;
+            this.MaxX = maxX;
+            this.MinY = minY;
+            this.MaxY = maxY;
+        }
+    }
+}
